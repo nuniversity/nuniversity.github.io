d48d9f895521b25909a9d5802e8242f30bc9e480
Leandro Alves <alves.engleandro@gmail.com>
2026-06-06 17:28:30 +1000

    fix next.config.js: ensure empty GITHUB_REPOSITORY doesn't produce invalid '/' basePath

--- next.config.js
+++ next.config.js
@@ -2,17 +2,17 @@
 
 const isProd = process.env.NODE_ENV === 'production'
 
-const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
+const repo = (process.env.GITHUB_REPOSITORY || '').split('/')[1] || ''
 const isOrgSite = repo.endsWith('.github.io')
-const basePath = isProd && !isOrgSite ? `/${repo}` : ''
+const basePath = isProd && repo && !isOrgSite ? '/' + repo : ''
 
 const nextConfig = {
   output: isProd ? 'export' : undefined,
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
   distDir: 'out',
-  assetPrefix: basePath ? `${basePath}/` : undefined,
-  basePath,
+  assetPrefix: basePath ? basePath + '/' : undefined,
+  basePath: basePath,
   images: {
     unoptimized: true,
   },
